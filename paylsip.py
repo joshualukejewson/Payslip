8d2ab1a18cd4c0cfc69c63c2f23b3d04c119ac05
Joshua Luke <115410856+joshualukejewson@users.noreply.github.com>
2022-12-02 12:03:26 +1000

    Added a time function that gathers the amount of hours worked that day.

--- paylsip.py
+++ paylsip.py
@@ -1,42 +1,66 @@
 import datetime
 import sys
 import csv
+import re
 
 
-class Shift():
+class Payslip():
+# BASE PAY RATE and SATURDAY and SUNDAY rate.
     BASE_RATE = 27.64
+    SATURDAY = 1.25
+    SUNDAY = 1.50
 
     def __init__(self):
-        self.total = 0
-        self.name = ""
+        self.total_earnt = 0
         self.total_hours_worked = 0
         self.total_tax = 0
 
+# Print payslip information
     def __str__(self):
         return f"""
     {self.name}
     Total hours worked: {self.total_hours_worked}
-    Total Payable: {self.total}
-    Total Tax Paid: {self.total_tax}
+    Total Payable: {self.total_earnt}
+    Total Tax Paid: {self.total_tax} 
     """
+# Set the name with the date format
+    def set_name(self, name):
+        self.name = name
 
+def main():
+
+    payslip = Payslip()
+    date = get_date()
+    payslip.name = f"Shift: {date}"
+
+    hours_worked = get_hours_worked()
+    print(f"{hours_worked} hours")
+
+    print(date)
+    print(payslip)
 
 def get_date():
     date = input("Enter date of shift (YYYY-MM-DD): ")
     return datetime.date.fromisoformat(date)
 
+def get_hours_worked():
 
-def main():
+    start_time = input("When did you start?: ")
+    matches_start = re.match(r"(2[0-4]|1[0-9]|[1-9])\:([0-5]?[0-9])", start_time)
+    end_time = input("When did you finish?: ")
+    matches_end = re.match(r"(2[0-4]|1[0-9]|[1-9])\:([0-5]?[0-9])", end_time)
 
-    shift = Shift()
+    start_hour = int(matches_start[1])
+    start_minutes = int(matches_start[2])
 
-    date = get_date()
-    shift.name = f"Shift: {date}"
+    end_hour = int(matches_end[1])
+    end_minutes = int(matches_end[2])
 
-    print(date)
-    print(shift)
-    ...
+    start_delta = datetime.timedelta(hours=start_hour, minutes=start_minutes)
+    end_delta = datetime.timedelta(hours=end_hour, minutes=end_minutes)
 
+    hours_diff = (end_delta - start_delta)
+    return (hours_diff.total_seconds() /60 / 60)
 
 if __name__ == "__main__":
     main()
